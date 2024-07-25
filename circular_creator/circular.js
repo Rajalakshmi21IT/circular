ac09616d9b21222426103bfb3e2c1bff01334597
Rajalakshmi21IT <130215690+Rajalakshmi21IT@users.noreply.github.com>
2024-07-26 00:29:23 +0530

    Update circular.js

--- circular_creator/circular.js
+++ circular_creator/circular.js
@@ -1,18 +1,18 @@
 function downloadPDF() {
     const { jsPDF } = window.jspdf;
 
-    html2canvas(document.getElementById('circular-content')).then(canvas => {
+    html2canvas(document.getElementById('circular-content'), { scale: 2 }).then(canvas => {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF('p', 'mm', 'a4');
         const pdfWidth = pdf.internal.pageSize.getWidth();
         const pdfHeight = pdf.internal.pageSize.getHeight();
         const imgWidth = pdfWidth;
-        let imgHeight = canvas.height * imgWidth / canvas.width;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
 
         let yOffset = 0;
         if (imgHeight > pdfHeight) {
             const scaleFactor = pdfHeight / imgHeight;
-            imgWidth = pdfWidth * scaleFactor;
+            imgWidth *= scaleFactor;
             imgHeight = pdfHeight;
             yOffset = 0;
         } else {
